dae9fcd49d944ed6f01f21a74d23916671471ab5
ramana910072 <ramana910072@gmail.com>
2023-12-08 21:22:47 +0530

    This is message micro services

---
+++ message/src/main/java/com/whatapp/message/exception/ResourceCreationException.java
@@ -0,0 +1,7 @@
+package com.whatapp.message.exception;
+
+public class ResourceCreationException extends RuntimeException{
+    public ResourceCreationException(String message){
+        super(message);
+    }
+}
